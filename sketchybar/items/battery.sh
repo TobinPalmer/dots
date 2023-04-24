2fd8739970b1ee9561c02b36faa7f1e4cc8702b8
Tobin Palmer <tobinkpalmer@gmail.com>
2023-04-24 16:56:48 -0700

    cleaned up sketcybar + yabai

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -3,5 +3,5 @@
 sketchybar -m --add item battery right \
               --set battery update_freq=3 \
               --set battery script="$PLUGIN_DIR/battery.sh" \
-              --set battery padding_right=-3 \
+              --set battery padding_right=3 \
               --set battery icon=
