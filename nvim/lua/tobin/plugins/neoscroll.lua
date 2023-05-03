c889e872d3945c2a2c2c0947bbe93712cfa9a891
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-03 11:56:57 -0700

    fixed neoscroll crashing on <C-u>

--- nvim/lua/tobin/plugins/neoscroll.lua
+++ nvim/lua/tobin/plugins/neoscroll.lua
@@ -15,14 +15,5 @@ return {
         performance_mode = false, -- Disable "Performance Mode" on all buffers.
       }
     end
-    local t = {}
-    t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', [['sine']] } }
-    t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', [['sine']] } }
-    t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']] } }
-    t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '500', [['circular']] } }
-    t['<C-y>'] = { 'scroll', { '-0.10', 'false', '100', nil } }
-    t['<C-e>'] = { 'scroll', { '0.10', 'false', '100', nil } }
-
-    require('neoscroll.config').set_mappings(t)
   end,
 }
