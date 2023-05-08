76adb023557c7ff963fafe187917d12c3a36a4af
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-07 19:31:12 -0700

    removed glitchy autocommand

--- nvim/lua/tobin/autocommands/init.lua
+++ nvim/lua/tobin/autocommands/init.lua
@@ -1,4 +1,4 @@
 require 'tobin.autocommands.comment'
 require 'tobin.autocommands.format'
 -- require 'tobin.autocommands.folds'
-require 'tobin.autocommands.set-cwd'
+-- require 'tobin.autocommands.set-cwd'
