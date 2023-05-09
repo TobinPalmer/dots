29fcb3525de7999d42dac4a362b64fe89810bf75
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-09 09:01:27 -0700

    added utils
    added a `util.sh` file that has some commonly used yabai variables to avoid redelcaring variables

--- skhd/scripts/yabai/move_space_left.sh
+++ skhd/scripts/yabai/move_space_left.sh
@@ -1,18 +1,12 @@
-SPACE_INDEX=$(yabai -m query --spaces | jq '.[] | select(."has-focus"==true).index')
-DISPLAY_NUMBER=$(yabai -m query --spaces | jq '.[] | select(."has-focus"==true).display')
-DISPLAY_INDEX_RANGE=$(yabai -m query --spaces | jq '.[] | select(."display"==1).index')
-DISPLAY_FIRST_NUMBER=$(printf %.1s "$DISPLAY_INDEX_RANGE")
-DISPLAY_LAST_NUMBER=$(echo ${DISPLAY_INDEX_RANGE:0-1})
+source "/Users/tobin/.config/skhd/scripts/yabai/util.sh"
 
-if [[ $SPACE_INDEX -eq $DISPLAY_FIRST_NUMBER ]]; then
+if [[ $CURRENT_SPACE_INDEX -eq $DISPLAY_FIRST_NUMBER ]]; then
   LEFT_SPACE_INDEX=$DISPLAY_LAST_NUMBER
 else
-  LEFT_SPACE_INDEX=$(($SPACE_INDEX - 1))
+  LEFT_SPACE_INDEX=$((CURRENT_SPACE_INDEX - 1))
 fi
 
-LOWEST_DISPLAYED_SPACE_INDEX=$(yabai -m query --spaces | jq '.[] | select(."visible"==1).index' | sort -n | head -n 1)
-
-yabai -m space --move $LEFT_SPACE_INDEX
+yabai -m space --move "$LEFT_SPACE_INDEX"
 
 sh ~/.config/yabai/create_spaces.sh
 
---
+++ skhd/scripts/yabai/util.sh
@@ -0,0 +1,7 @@
+export LOWEST_DISPLAYED_SPACE_INDEX=$(yabai -m query --spaces | jq '.[] | select(."visible"==1).index' | sort -n | head -n 1)
+export CURRENT_SPACE_INDEX=$(yabai -m query --spaces | jq '.[] | select(."has-focus"==true).index')
+export CURRENT_DISPLAY_NUMBER=$(yabai -m query --spaces | jq '.[] | select(."has-focus"==true).display')
+export DISPLAY_INDEX_RANGE=$(yabai -m query --spaces | jq '.[] | select(."display"==1).index')
+export DISPLAY_FIRST_NUMBER=$(printf %.1s "$DISPLAY_INDEX_RANGE")
+export DISPLAY_LAST_NUMBER=$(echo ${DISPLAY_INDEX_RANGE:0-1})
+
