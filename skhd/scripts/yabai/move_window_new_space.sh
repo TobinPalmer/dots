5e8e437931c8afc388afe667def0e9d677f5c95e
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-14 12:26:34 -0700

    sorta working skhd config

--- skhd/scripts/yabai/move_window_new_space.sh
+++ skhd/scripts/yabai/move_window_new_space.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+source "/Users/tobin/.config/skhd/scripts/yabai/util.sh"
 
+# yabai -m space --create && \
+#                    index="$(yabai -m query --spaces --display | jq 'map(select(."is-native-fullscreen" == false))[-1].index')" && \
+#                    yabai -m window --space "${index}" && \
+#                    yabai -m space --focus "${index}"
 yabai -m space --create &&
 yabai -m window --space last &&
 yabai -m space --focus last
+
+# wallpaper set ~/.config/wallpapers/Arch.jpg
+sh ~/.config/yabai/create_spaces.sh
--- skhd/scripts/yabai/move_window_new_space_no_switch.sh
+++ skhd/scripts/yabai/move_window_new_space_no_switch.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+source "/Users/tobin/.config/skhd/scripts/yabai/util.sh"
+
+# yabai -m space --create && \
+#                    index="$(yabai -m query --spaces --display | jq 'map(select(."is-native-fullscreen" == false))[-1].index')" && \
+#                    yabai -m window --space "${index}" && \
+#                    yabai -m space --focus "$CURRENT_SPACE_INDEX"
+
 yabai -m space --create &&
 yabai -m window --space last
+
+# wallpaper set ~/.config/wallpapers/Arch.jpg
+sh ~/.config/yabai/create_spaces.sh
