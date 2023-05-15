3828e11e2f56109302881b1600b1f00c43cf70d2
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-14 17:20:42 -0700

    respaced sketchybar elements
    Made elements on sketchybar have more consistant spacing

--- sketchybar/items/wifi.sh
+++ sketchybar/items/wifi.sh
@@ -6,7 +6,8 @@ sketchybar --add alias  "Control Center,WiFi" right                      \
            --rename     "Control Center,WiFi" wifi.alias                 \
            --set        wifi.alias    icon.drawing=off                   \
                                       alias.color="$YELLOW"              \
-                                      background.padding_right=0         \
+                                      background.padding_right=-10       \
+                                      background.padding_left=-10        \
                                       icon.padding_right=0               \
                                       align=right                        \
                                       click_script="$POPUP_CLICK_SCRIPT" \
@@ -23,7 +24,7 @@ sketchybar --add alias  "Control Center,WiFi" right                      \
                                       icon.background.height=2           \
                                       icon.background.y_offset=-12       \
                                       label.align=center                 \
-            click_script="sketchybar --set wifi.alias popup.drawing=off"
+          click_script="sketchybar --set wifi.alias popup.drawing=off"
 #
 # sketchybar --add alias  "Control Center,WiFi" right                      \
 #            --rename     "Control Center,WiFi" wifi.alias                 \
