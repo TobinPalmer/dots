0adce0544d532441abcc48554aba83cd4ee6de49
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-16 10:09:35 -0700

    new highlighting

--- nvim/lua/tobin/highlights/keyword.lua
+++ nvim/lua/tobin/highlights/keyword.lua
@@ -1,1 +1,8 @@
-vim.cmd [[ highlight Keyword gui=bold,italic cterm=bold,italic ]]
+vim.cmd [[
+highlight Keyword gui=italic cterm=italic 
+highlight Special gui=italic cterm=italic 
+highlight typescriptCase gui=italic cterm=italic 
+highlight StorageClass gui=italic cterm=italic 
+highlight Structure gui=italic cterm=italic 
+highlight Statement gui=italic cterm=italic 
+]]
