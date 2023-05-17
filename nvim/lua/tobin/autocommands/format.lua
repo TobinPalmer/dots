d479142be475e4579fd4abe19a3449811bdc6143
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-16 18:31:36 -0700

    new formatting autocommand

--- nvim/lua/tobin/autocommands/format.lua
+++ nvim/lua/tobin/autocommands/format.lua
@@ -1,2 +1,7 @@
 local format = require 'tobin.utils.format'
 -- vim.cmd [[autocmd bufreadpost * lua format()]]
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    format.format()
+  end,
+})
