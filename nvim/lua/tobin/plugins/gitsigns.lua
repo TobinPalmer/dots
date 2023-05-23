9fec22909678a39ef65f8ef8a5d6e5ef2495e6ee
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-23 10:10:43 -0700

    removed clashing gitsigns binding

--- nvim/lua/tobin/plugins/gitsigns.lua
+++ nvim/lua/tobin/plugins/gitsigns.lua
@@ -6,7 +6,6 @@ return {
     { 'gN', '<cmd>Gitsigns prev_hunk<CR>' },
     { '<leader>gb', '<cmd>Gitsigns blame_line<CR>' },
     { '<leader>gl', '<cmd>Gitsigns toggle_current_line_blame<CR>' },
-    { '<leader>gd', '<cmd>Gitsigns toggle_deleted<CR>' },
     { '<leader>gw', '<cmd>Gitsigns toggle_word_diff<CR>' },
   },
   opts = {
