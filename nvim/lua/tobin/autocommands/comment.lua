c6105f112caf6e661014f5b1d07be4545527fde2
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-23 10:08:59 -0700

    removed testing vim.cmd

--- nvim/lua/tobin/autocommands/comment.lua
+++ nvim/lua/tobin/autocommands/comment.lua
@@ -4,9 +4,4 @@ vim.cmd [[
     autocmd FileType * setlocal formatoptions-=cro
     autocmd FileType * setlocal comments-=:#
   augroup END
-]]
-
-vim.cmd [["Highlighting]]
-vim.cmd [[
-" No highlighting
-]]
+  ]]
