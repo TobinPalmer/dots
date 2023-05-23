6bd72ba5dd6daaa3aaf5b16bcb8ff5a89b72a056
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-23 12:11:35 -0700

    updated folds to not set pwd

--- nvim/lua/tobin/autocommands/folds.lua
+++ nvim/lua/tobin/autocommands/folds.lua
@@ -1,7 +1,13 @@
-vim.cmd [[
-augroup remember_folds
-  autocmd!
-  autocmd BufWinLeave *.* silent! mkview %
-  autocmd BufWinEnter *.* silent! loadview %
-augroup END
-]]
+local cwd = vim.fn.getcwd()
+
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  pattern = '*.*',
+  command = 'mkview',
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  callback = function()
+    vim.cmd [[ silent! loadview ]]
+    vim.fn.execute('cd ' .. cwd)
+  end,
+})
