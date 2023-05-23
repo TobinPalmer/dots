0ce5196549b0fb2a14bc2699ae8831402b8c3c0d
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-23 11:26:42 -0700

    fixed folds.lua changing the current working direcotry

--- nvim/lua/tobin/autocommands/folds.lua
+++ nvim/lua/tobin/autocommands/folds.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
 augroup remember_folds
   autocmd!
-  autocmd BufWinLeave *.* mkview
-  autocmd BufWinEnter *.* silent! loadview
+  autocmd BufWinLeave *.* silent! mkview %
+  autocmd BufWinEnter *.* silent! loadview %
 augroup END
 ]]
