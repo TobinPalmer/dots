3ca6efde42455b83d29bdb990c47a8781e3f0c26
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-23 19:02:12 -0700

    updated drop.nvim config

--- nvim/lua/tobin/plugins/ui/drop.lua
+++ nvim/lua/tobin/plugins/ui/drop.lua
@@ -3,11 +3,10 @@ return {
   event = 'VimEnter',
   config = function()
     require('drop').setup {
-      ---@type DropTheme|string
-      theme = 'snow', -- can be one of rhe default themes, or a custom theme
-      max = 40, -- maximum number of drops on the screen
-      interval = 150, -- every 150ms we update the drops
-      screensaver = 3 * 1000 * 60, -- show after 5 minutes. Set to false, to disable
+      theme = 'summer',
+      max = 50,
+      interval = 150,
+      screensaver = 3 * 1000 * 60,
       filetypes = {}, -- will enable/disable automatically for the following filetypes
     }
   end,
