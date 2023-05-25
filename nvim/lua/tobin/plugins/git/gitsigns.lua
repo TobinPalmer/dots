cb21f12922c1b5bb1259a5eaf760ffcc2d7eed5a
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-24 21:34:44 -0700

    changed gitsigns config

--- nvim/lua/tobin/plugins/git/gitsigns.lua
+++ nvim/lua/tobin/plugins/git/gitsigns.lua
@@ -1,12 +1,13 @@
 return {
   'lewis6991/gitsigns.nvim',
-  cmd = { 'Gitsigns' },
+  event = 'BufReadPost',
   keys = {
     { 'gn', '<cmd>Gitsigns next_hunk<CR>' },
     { 'gN', '<cmd>Gitsigns prev_hunk<CR>' },
     { '<leader>gb', '<cmd>Gitsigns blame_line<CR>' },
     { '<leader>gl', '<cmd>Gitsigns toggle_current_line_blame<CR>' },
     { '<leader>gw', '<cmd>Gitsigns toggle_word_diff<CR>' },
+    { '<leader>gd', '<cmd>Gitsigns diffthis<CR>' },
   },
   opts = {
     signs = {
@@ -16,4 +17,9 @@ return {
       changedelete = { text = 'D' },
     },
   },
+  config = function()
+    require('gitsigns').setup {
+      current_line_blame = true,
+    }
+  end,
 }
