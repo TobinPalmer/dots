a8fdc7523eca56f06220443b31ec254e2c46b06e
TobinPalmer <tobinkpalmer@gmail.com>
2023-05-26 13:17:14 -0700

    new vim switcher function

--- .zsh/functions/vim.sh
+++ .zsh/functions/vim.sh
@@ -1,29 +1,29 @@
-avim() {
-    if [ ! -r ~/.local/share/nvim/readme.md ]; then
+mini() {
+    if [ ! -r ~/.local/share/nvim/README.md ]; then
         echo "Error: File does not exist or is not readable"
         return 1
     fi
 
-    local_file=$(cat ~/.local/share/nvim/readme.md)
-    config_file=$(cat ~/.config/nvim/readme.md)
+    local_file=$(cat ~/.local/share/nvim/README.md)
+    config_file=$(cat ~/.config/nvim/README.md)
 
-    if [[ "$local_file" == *"astrovim"* ]]; then # Astrovim is in local
-        if [[ "$config_file" == *"astrovim"* ]]; then # Config is astrovim
+    if [[ "$local_file" == *"minimal"* ]]; then # Minimal is in local
+        if [[ "$config_file" == *"minimal"* ]]; then # Config is minimal
             nvim;
         elif [[ "$config_file" == *"neovim"* ]]; then # Config is Neovim
             mv ~/.config/nvim ~/.config/neovim
-            mv ~/.config/avim ~/.config/nvim
+            mv ~/.config/minimal ~/.config/nvim
             nvim;
         fi
         return 0
     elif [[ "$local_file" == *"neovim"* ]]; then # Neovim is in local
-        if [[ "$config_file" == *"astrovim"* ]]; then # Config is in astrovim
+        if [[ "$config_file" == *"minimal"* ]]; then # Config is in minimal
             nvim;
         elif [[ "$config_file" == *"neovim"* ]]; then # Both configs are neovim
             mv ~/.config/nvim ~/.config/neovim
-            mv ~/.config/avim ~/.config/nvim
+            mv ~/.config/minimal ~/.config/nvim
             mv ~/.local/share/nvim ~/.local/share/neovim
-            mv ~/.local/share/avim ~/.local/share/nvim
+            mv ~/.local/share/minimal ~/.local/share/nvim
             nvim;
         fi
         return 0
@@ -34,30 +34,30 @@ avim() {
 }
 
 neovim() {
-    if [ ! -r ~/.local/share/nvim/readme.md ]; then
+    if [ ! -r ~/.local/share/nvim/README.md ]; then
         echo "Error: File does not exist or is not readable"
         return 1
     fi
 
-    local_file=$(cat ~/.local/share/nvim/readme.md)
-    config_file=$(cat ~/.config/nvim/readme.md)
+    local_file=$(cat ~/.local/share/nvim/README.md)
+    config_file=$(cat ~/.config/nvim/README.md)
 
-    if [[ "$local_file" == *"astrovim"* ]]; then # Astrovim is in local
-        if [[ "$config_file" == *"astrovim"* ]]; then # Config is astrovim
-            mv ~/.config/nvim ~/.config/avim
+    if [[ "$local_file" == *"minimal"* ]]; then # Minimal is in local
+        if [[ "$config_file" == *"minimal"* ]]; then # Config is minimal
+            mv ~/.config/nvim ~/.config/minimal
             mv ~/.config/neovim ~/.config/nvim
-            mv ~/.local/share/nvim ~/.local/share/avim
+            mv ~/.local/share/nvim ~/.local/share/minimal
             mv ~/.local/share/neovim ~/.local/share/nvim
             nvim;
         elif [[ "$config_file" == *"neovim"* ]]; then # Config is Neovim
-            mv ~/.local/share/nvim ~/.local/share/avim
+            mv ~/.local/share/nvim ~/.local/share/minimal
             mv ~/.local/share/neovim ~/.local/share/nvim
             nvim;
         fi
         return 0
     elif [[ "$local_file" == *"neovim"* ]]; then # Neovim is in local
-        if [[ "$config_file" == *"astrovim"* ]]; then # Config is in astrovim
-            mv ~/.config/nvim ~/.config/avim
+        if [[ "$config_file" == *"minimal"* ]]; then # Config is in minimal
+            mv ~/.config/nvim ~/.config/minimal
             mv ~/.config/neovim ~/.config/nvim
             nvim;
         elif [[ "$config_file" == *"neovim"* ]]; then # Both configs are neovim
