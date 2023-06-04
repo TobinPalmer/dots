43204d2aea44b997e7b24a7e98fa7233d9bf2b6d
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-04 08:24:06 -0700

    converted to lua

--- nvim/lua/tobin/autocommands/comment.lua
+++ nvim/lua/tobin/autocommands/comment.lua
@@ -1,7 +1,6 @@
-vim.cmd [[
-  augroup disable_comment_continuation
-    autocmd!
-    autocmd FileType * setlocal formatoptions-=cro
-    autocmd FileType * setlocal comments-=:#
-  augroup END
-]]
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.cmd [[ setlocal formatoptions-=cro ]]
+    vim.cmd [[ setlocal comments-=:#]]
+  end,
+})
