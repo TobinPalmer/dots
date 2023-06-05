bd06c01750f97918354605510ab4322ce9e3bbef
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-05 07:52:39 -0700

    lazy load FTerm

--- nvim/lua/tobin/plugins/misc/FTerm.lua
+++ nvim/lua/tobin/plugins/misc/FTerm.lua
@@ -1,14 +1,16 @@
 return {
   'numToStr/FTerm.nvim',
+  keys = {
+    { '<leader>mt', '<CMD>lua require("FTerm").toggle()<CR>', desc = 'Toggle Term' },
+  },
   config = function()
     require('FTerm').setup {
       border = 'double',
       dimensions = {
-        height = 0.9,
-        width = 0.9,
+        height = 0.85,
+        width = 0.85,
       },
     }
-    vim.keymap.set('n', '<leader>mt', '<CMD>lua require("FTerm").toggle()<CR>')
     vim.keymap.set('t', '<ESC>h', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
   end,
 }
