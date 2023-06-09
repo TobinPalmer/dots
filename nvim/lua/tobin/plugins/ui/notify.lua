2adfc16f6b64bc5d55b27cd99f81384ea7c18fd1
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-08 20:56:27 -0700

    new notify config + keybind

--- nvim/lua/tobin/plugins/ui/notify.lua
+++ nvim/lua/tobin/plugins/ui/notify.lua
@@ -1,14 +1,25 @@
 return {
   'rcarriga/nvim-notify',
   event = 'BufReadPost',
+  keys = {
+    { '<leader>nd', '<CMD>Noice dismiss<CR>' },
+  },
   config = function()
     local notify = require 'notify'
 
     vim.notify = function(message, level, opts)
       return notify(message, level, opts) -- <-- Important to return the value from `nvim-notify`
     end
+
     require('notify').setup {
-      background_colour = '#000000',
+      -- background_colour = '#000000',
+      fps = 144,
+      minimum_width = 50,
+      max_width = 100,
+      render = 'default',
+      stages = 'fade_in_slide_out',
+      timeout = 0,
+      top_down = true,
     }
   end,
 }
