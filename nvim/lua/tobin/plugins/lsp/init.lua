9390bf21870fe97a67030d72d779f14066b2765f
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-09 22:26:23 -0700

    new plugin - barbecue

--- nvim/lua/tobin/plugins/lsp/init.lua
+++ nvim/lua/tobin/plugins/lsp/init.lua
@@ -1,14 +1,15 @@
 local plugins = {
-  require 'tobin.plugins.lsp.treesitter',
   require 'tobin.plugins.lsp.cmp',
   require 'tobin.plugins.lsp.lsp',
-  require 'tobin.plugins.lsp.typescript',
   require 'tobin.plugins.lsp.copilot',
   require 'tobin.plugins.lsp.lspsaga',
   require 'tobin.plugins.lsp.null_ls',
-  require 'tobin.plugins.lsp.mason-tools',
+  require 'tobin.plugins.lsp.barbecue',
   require 'tobin.plugins.lsp.lsp-config',
+  require 'tobin.plugins.lsp.treesitter',
+  require 'tobin.plugins.lsp.typescript',
   require 'tobin.plugins.lsp.inlay-hints',
+  require 'tobin.plugins.lsp.mason-tools',
 }
 
 if not vim.g.opts.minimal then
