cbecfe5144ac97c0966e3b6d9967e6c2a562a964
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-12 11:23:58 -0700

    changed color file to be more simple

--- nvim/lua/tobin/plugins/ui/color.lua
+++ nvim/lua/tobin/plugins/ui/color.lua
@@ -1,73 +1,20 @@
-return {
-  {
-    'Mofiqul/dracula.nvim',
-    event = 'BufReadPost',
-    -- priority = 1000,
-    config = function()
-      require('tokyonight').setup {}
-      -- vim.cmd.colorscheme 'dracula'
-    end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    -- event = 'BufReadPost',
-    priority = 1000,
-    config = function()
-      require('tokyonight').setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        light_style = 'day', -- The theme is used when the background is set to light
-        transparent = false, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = 'transparent', -- style for sidebars, see below
-          floats = 'transparent', -- style for floating windows
-        },
-        sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = true, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-      }
-      vim.cmd.colorscheme 'tokyonight-moon'
-    end,
-  },
-  {
-    'ellisonleao/gruvbox.nvim',
-    -- priority = 1000,
-    event = 'BufReadPost',
-    config = function()
-      require('gruvbox').setup {
-        overrides = {},
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = 'hard', -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        dim_inactive = true,
-        transparent_mode = true,
-      }
-      -- vim.cmd 'colorscheme gruvbox'
-    end,
-  },
+local colorsscheme = {
+  { name = 'folke/tokyonight.nvim', theme = 'tokyonight', enabled = true },
+  { name = 'ellisonleao/gruvbox.nvim', theme = 'gruvbox', enabled = false },
+  { name = 'Mofiqul/dracula.nvim', theme = 'dracula', enabled = false },
 }
+
+local object = {}
+
+for _, scheme in pairs(colorsscheme) do
+  table.insert(object, { scheme.name, cond = scheme.enabled })
+  if scheme.enabled then
+    vim.api.nvim_create_autocmd('UIEnter', {
+      callback = function()
+        vim.cmd('colorscheme ' .. scheme.theme)
+      end,
+    })
+  end
+end
+
+return object
