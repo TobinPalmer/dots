ce84afb9104b456d5f6f39272688fe1145abf2dc
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-20 11:46:32 -0700

    changed overseer config

--- nvim/lua/overseer/template/tobin/typescript.lua
+++ nvim/lua/overseer/template/tobin/typescript.lua
@@ -6,6 +6,7 @@ return {
       components = {
         'default',
         { 'on_output_parse', problem_matcher = '$tsc-watch' },
+        'restart_on_save',
         'on_result_diagnostics',
         'on_result_diagnostics_quickfix',
       },
