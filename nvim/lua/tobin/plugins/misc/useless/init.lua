72a1e527233b00209f0e256aabbfdd3d64887de7
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-24 18:17:54 -0700

    added colorizer plugin

--- nvim/lua/tobin/plugins/misc/useless/init.lua
+++ nvim/lua/tobin/plugins/misc/useless/init.lua
@@ -2,5 +2,6 @@ return {
   require 'tobin.plugins.misc.useless.tetris',
   require 'tobin.plugins.misc.useless.modicator',
   require 'tobin.plugins.misc.useless.neoscroll',
+  require 'tobin.plugins.misc.useless.colorizer',
   require 'tobin.plugins.misc.useless.cellular-automaton',
 }
