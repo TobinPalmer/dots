2bb9b0bfc36b16d70775e883fd3350ff37666b1b
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-27 10:40:40 -0700

    changed flash.nvim config

--- nvim/lua/tobin/plugins/productivity/flash.lua
+++ nvim/lua/tobin/plugins/productivity/flash.lua
@@ -1,24 +1,40 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
+  ---@type Flash.Config
   opts = {},
   keys = {
     {
       '<S-CR>',
       mode = { 'n', 'x', 'o' },
       function()
-        -- default options: exact mode, multi window, all directions, with a backdrop
         require('flash').jump()
       end,
     },
     {
-      '*',
+      's',
       mode = { 'n', 'x', 'o' },
       function()
-        require('flash').jump {
-          pattern = vim.fn.expand '<cword>',
-        }
+        -- default options: exact mode, multi window, all directions, with a backdrop
+        require('flash').jump()
       end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
     },
   },
 }
