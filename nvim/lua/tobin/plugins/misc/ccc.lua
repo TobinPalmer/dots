6fadc264c1cbd719278c115dfe8aac9b5a675a1a
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-28 16:46:32 -0700

    added bindings to ccc.nvim

--- nvim/lua/tobin/plugins/misc/ccc.lua
+++ nvim/lua/tobin/plugins/misc/ccc.lua
@@ -1,6 +1,10 @@
 return {
   'uga-rosa/ccc.nvim',
   event = 'BufReadPost',
+  keys = {
+    { '<leader>ca', '<CMD>CccPick<CR>' },
+    { '<leader>cc', '<CMD>CccConvert<CR>' },
+  },
   config = function()
     local ccc = require 'ccc'
 
