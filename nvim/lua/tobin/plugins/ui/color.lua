ab3e97a5af3ff2a77aa1342f939c7af2851234d1
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-28 16:46:53 -0700

    added catpuccin and kanagawa.nvim

--- nvim/lua/tobin/plugins/ui/color.lua
+++ nvim/lua/tobin/plugins/ui/color.lua
@@ -1,8 +1,9 @@
 local colorsscheme = {
   { name = 'folke/tokyonight.nvim', theme = 'tokyonight-night', enabled = false },
-  { name = 'rebelot/kanagawa.nvim', theme = 'kanagawa', enabled = true },
+  { name = 'rebelot/kanagawa.nvim', theme = 'kanagawa', enabled = false },
   { name = 'ellisonleao/gruvbox.nvim', theme = 'gruvbox', enabled = false },
   { name = 'Mofiqul/dracula.nvim', theme = 'dracula', enabled = false },
+  { name = 'catppuccin/nvim', theme = 'catppuccin-mocha', enabled = true },
 }
 
 local object = {}
