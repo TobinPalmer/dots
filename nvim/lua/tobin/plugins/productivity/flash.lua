ff47e5e41177e93cc322385e85b489f15fda7795
TobinPalmer <tobinkpalmer@gmail.com>
2023-06-29 13:29:55 -0700

    removed s binding in flash

--- nvim/lua/tobin/plugins/productivity/flash.lua
+++ nvim/lua/tobin/plugins/productivity/flash.lua
@@ -12,15 +12,6 @@ return {
       end,
     },
     {
-      's',
-      mode = { 'n', 'x', 'o' },
-      function()
-        -- default options: exact mode, multi window, all directions, with a backdrop
-        require('flash').jump()
-      end,
-      desc = 'Flash',
-    },
-    {
       'S',
       mode = { 'n', 'o', 'x' },
       function()
