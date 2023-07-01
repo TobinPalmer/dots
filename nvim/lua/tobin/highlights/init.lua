c886b343d17df65128b57e7b4057beceba716e1e
TobinPalmer <tobinkpalmer@gmail.com>
2023-07-01 12:42:53 -0700

    added new method of highlighting

--- nvim/lua/tobin/highlights/init.lua
+++ nvim/lua/tobin/highlights/init.lua
@@ -1,8 +1,34 @@
-vim.cmd [[
-highlight Keyword gui=italic,bold cterm=italic,bold
-highlight Special gui=italic,bold cterm=italic,bold
-highlight typescriptCase gui=italic,bold cterm=italic,bold
-highlight StorageClass gui=italic,bold cterm=italic,bold
-highlight Structure gui=italic,bold cterm=italic,bold
-highlight Statement gui=italic,bold cterm=italic,bold
-]]
+local function update_hl(group, tbl)
+  local old_hl = vim.api.nvim_get_hl_by_name(group, true)
+  local new_hl = vim.tbl_extend('force', old_hl, tbl)
+  vim.api.nvim_set_hl(0, group, new_hl)
+end
+
+local italic = {
+  'Keyword',
+  '@keyword.function',
+  '@keyword.return',
+  'Type',
+  '@type.builtin',
+}
+
+local bold = {}
+
+local italic_and_bold = {
+  'Special',
+  'StorageClass',
+  'Structure',
+  'Statement',
+}
+
+for _, group in ipairs(italic) do
+  update_hl(group, { italic = true })
+end
+
+for _, group in ipairs(bold) do
+  update_hl(group, { bold = true })
+end
+
+for _, group in ipairs(italic_and_bold) do
+  update_hl(group, { italic = true, bold = true })
+end
