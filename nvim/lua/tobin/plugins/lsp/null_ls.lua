36f293c90403efb419cbcf519d3c3c8c21e842fd
TobinPalmer <tobinkpalmer@gmail.com>
2023-09-30 16:29:59 -0700

    fixed null_ls crashing with eslint

--- nvim/lua/tobin/plugins/lsp/null_ls.lua
+++ nvim/lua/tobin/plugins/lsp/null_ls.lua
@@ -10,22 +10,16 @@ return {
       null_ls.builtins.completion.spell,
       null_ls.builtins.diagnostics.vint,
       null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.diagnostics.eslint,
       null_ls.builtins.diagnostics.gitlint,
       null_ls.builtins.formatting.beautysh,
       null_ls.builtins.formatting.bibclean,
       null_ls.builtins.formatting.prettier,
-      null_ls.builtins.code_actions.eslint_d,
       null_ls.builtins.code_actions.gitsigns,
       null_ls.builtins.diagnostics.write_good,
       null_ls.builtins.diagnostics.mdl,
-      -- null_ls.builtins.formatting.markdownlint,
-      -- null_ls.builtins.diagnostics.proselint,
       null_ls.builtins.code_actions.refactoring,
-      -- null_ls.builtins.diagnostics.codespell,
       null_ls.builtins.formatting.codespell,
       null_ls.builtins.code_actions.proselint,
-      -- null_ls.builtins.diagnostics.vale,
       null_ls.builtins.diagnostics.vale,
       null_ls.builtins.formatting.black.with {
         extra_args = { '--line-length=120' },
@@ -33,8 +27,8 @@ return {
     }
 
     local eslintSources = {
-      -- null_ls.builtins.diagnostics.eslint,
-      -- null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.code_actions.eslint_d,
     }
 
     if
@@ -42,7 +36,7 @@ return {
       or vim.fn.filereadable '.eslintrc.json' == 1
       or vim.fn.filereadable '.eslintrc.js' == 1
     then
-      table.insert(sources, eslintSources)
+      table.insert(eslintSources, sources)
     end
 
     null_ls.setup {
