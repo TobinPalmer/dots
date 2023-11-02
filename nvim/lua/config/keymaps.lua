f81a595850cb5fcd8c7bf873e1fb261ea1874f07
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-02 14:36:24 -0700

    Added lazygit + optimize mappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,18 +1,3 @@
 vim.opt_local.formatoptions:remove({ "r", "o" })
 
 local map = vim.keymap.set
-
--- Buffers
--- map("n", "<leader>bb", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
--- map("n", "<leader>bc", "<cmd>BufferLineCloseOthers<cr>", { desc = "Prev buffer" })
--- map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
--- map("n", "<leader>b1", '<CMD>lua require("bufferline").go_to(1, true)<CR>', { desc = "Go to buffer 1" })
--- map("n", "<leader>b2", '<CMD>lua require("bufferline").go_to(2, true)<CR>', { desc = "Go to buffer 2" })
--- map("n", "<leader>b3", '<CMD>lua require("bufferline").go_to(3, true)<CR>', { desc = "Go to buffer 3" })
--- map("n", "<leader>b4", '<CMD>lua require("bufferline").go_to(4, true)<CR>', { desc = "Go to buffer 4" })
--- map("n", "<leader>b5", '<CMD>lua require("bufferline").go_to(5, true)<CR>', { desc = "Go to buffer 5" })
--- map("n", "<leader>b6", '<CMD>lua require("bufferline").go_to(6, true)<CR>', { desc = "Go to buffer 6" })
--- map("n", "<leader>b7", '<CMD>lua require("bufferline").go_to(7, true)<CR>', { desc = "Go to buffer 7" })
--- map("n", "<leader>b8", '<CMD>lua require("bufferline").go_to(8, true)<CR>', { desc = "Go to buffer 8" })
--- map("n", "<leader>b9", '<CMD>lua require("bufferline").go_to(9, true)<CR>', { desc = "Go to buffer 9" })
--- map("n", "<leader>b$", '<CMD>lua require("bufferline").go_to(-1, true)<CR>', { desc = "Go to last buffer" })
