a00f3768bb13b9b3519f3c9990bd1c9e06b2e160
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-05 21:52:23 -0800

    added terminal keybindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,14 @@
+local Util = require("lazyvim.util")
 local map = vim.keymap.set
 
 map("n", "<leader>ga", vim.lsp.buf.code_action, { silent = true })
 map("n", "<leader>gr", vim.lsp.buf.rename, { silent = true })
+
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+
+map("n", "<leader>mt", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>mT", function()
+  Util.terminal()
+end, { desc = "Terminal (cwd)" })
