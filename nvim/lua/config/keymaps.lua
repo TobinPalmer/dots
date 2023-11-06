3610392ec683e6c14ebef6f39936ffc83607c4fe
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-05 19:16:38 -0800

    Added LSP keybindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
-vim.opt_local.formatoptions:remove({ "r", "o" })
-
 local map = vim.keymap.set
+
+map("n", "<leader>ga", vim.lsp.buf.code_action, { silent = true })
+map("n", "<leader>gr", vim.lsp.buf.rename, { silent = true })
