394e86cbae954bcb8b13d10fad76d66c2ae31108
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-30 19:30:59 -0800

    added presence.nvim

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -3,6 +3,26 @@ local leet_arg = "leetcode.nvim"
 return {
   { "wakatime/vim-wakatime", lazy = false },
   {
+    "andweeb/presence.nvim",
+    lazy = false,
+    config = function()
+      require("presence").setup({
+        auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+        neovim_image_text = "Neovim",
+        main_image = "neovim", -- Main image display (either "neovim" or "file")
+
+        -- Rich Presence text options
+        editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+        file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+        git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+        plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+        reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+        workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+        line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+      })
+    end,
+  },
+  {
     "kawre/leetcode.nvim",
     lazy = leet_arg ~= vim.fn.argv()[1],
     build = ":TSUpdate html",
